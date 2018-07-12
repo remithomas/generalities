c4df7a8ac6134e7a372fed521340253ec89e2fd7
Rémi THOMAS <remi.thomas@yahoo.fr>
2018-07-12 08:26:07 -0400

    Mine types + Extensions (#7)

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 import * as CountryCodes from './country-codes';
+import * as Extensions from './extensions';
 import * as HttpCodes from './http-codes';
 import * as HttpMethods from './http-methods';
 import * as LanguageCodes from './language-codes';
+import * as MimeTypes from './mime-types';
 
 export {
     CountryCodes,
+    Extensions,
     HttpCodes,
     HttpMethods,
     LanguageCodes,
+    MimeTypes,
 };
